a12c8e61452888571ee48417258f9992a468c8eb
shige523 <amashige0709@gmail.com>
2021-07-05 15:19:02 +0900

    resolve SettingWithCopyWarning warning

--- agg_booklogcsv.py
+++ agg_booklogcsv.py
@@ -3,10 +3,16 @@ import pandas as pd
 """
 [ブクログでexpしたcsvデータを年月日別＆ジャンル別に集計]
 [参考URL]
+--pandas.DataFrame, Seriesを時系列データとして処理
+https://note.nkmk.me/python-pandas-time-series-datetimeindex/
 --pandasで時系列データの曜日や月、四半期、年ごとの合計や平均を算出
 https://note.nkmk.me/python-pandas-time-series-multiindex/
 --Pandasのgroupbyを使った要素をグループ化して処理をする方法
 https://deepage.net/features/pandas-groupby.html
+--pandas の SettingWithCopyWarning で苦労した話
+https://qiita.com/HEM_SP/items/56cd62a1c000d342bd70
+--pandasのgroupbyでグループ化した文字列を結合する
+https://qiita.com/ground0state/items/2fb8d1938220df8a0194
 """
 
 
@@ -45,7 +51,7 @@ def agg_execute(filename):
         (book_log_df["読書状況"] == "読み終わった")
         & (book_log_df["読了日"] != None)
         & (book_log_df["読了日"] != "0000-00-00 00:00:00")
-    ]
+    ].copy()
 
     # 読了日を時系列データとして設定
     filter_df["読了日"] = pd.to_datetime(filter_df["読了日"])
@@ -56,14 +62,24 @@ def agg_execute(filename):
         [filter_df.index.year, filter_df.index.month, filter_df.index]
     )
     df_m.index.names = ["year", "month", "読了日"]
-    # 設定したインデックスを用いてgroupby
-    df_m_by_genre = df_m[["ジャンル"]].groupby(["year", "month", "ジャンル"]).size()
+
+    # 月ごとのジャンル内訳
+    df_m_by_genre = (
+        df_m[["ジャンル"]].groupby(["year", "month", "ジャンル"]).size().sort_index()
+    )
     df_m_by_genre.name = "冊"
     df_m_by_genre.to_csv("result_count_by_genre_yyyymm.csv", encoding="utf-8")
 
+    # 月ごとのページ数合計
     df_m_by_page = df_m["ページ数"].sum(level=("year", "month")).sort_index()
     df_m_by_page.to_csv("result_page_sum.csv", encoding="utf-8")
 
+    # 月ごとのタイトル一覧
+    df_m_by_title = df_m["タイトル"].sum(level=("year", "month"))
+    df_m_by_title = df_m["タイトル"].groupby(level=("year", "month")).apply(list)
+    df_m_by_title.to_csv("result_title_sum.csv", encoding="utf-8")
+
 
 if __name__ == "__main__":
+    # 読み込むcsvを指定
     agg_execute("booklog.csv")
